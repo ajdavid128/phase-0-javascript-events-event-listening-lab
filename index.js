c8488ba9fd249c568c4295acf266cf31ab7809ca
Aaron David <ajdavid128@gmail.com>
2022-10-03 11:04:54 -0500

    added click gameboard table, added javascript listening events for gameboard, added css attributes to gameboard table

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
 }
+addingEventListener();
+//This needs to be included or the event function won't be called
